e395f2006c3ce9e78dbbbbe1692031e8237d9a83
H-strangeone <hiteshgitman@gmail.com>
2025-11-02 16:12:08 +0530

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ use std::io::{self, Write};
 
 fn main() {
     // Uncomment this block to pass the first stage
-    print!("$ ");
+    
+    // Wait for user input
+    loop{
+        print!("$ ");
     
     io::stdout().flush().unwrap();
 
-    // Wait for user input
-    loop{
         let stdin=io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
