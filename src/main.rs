220a36dae32499d846bbba15b2db5e2929fd8e7b
H-strangeone <hiteshgitman@gmail.com>
2025-11-03 04:08:48 +0530

    added type

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::collections::HashSet;
 
+
+        
 fn main() {
     // Uncomment this block to pass the first stage
     
@@ -16,6 +19,16 @@ fn main() {
         let input=input.trim();
         if input.is_empty(){
             continue;
+        }
+        let builtin_cmds:HashSet<&str>=["exit","quit","help","exit 0","quit 0","exit 1","echo","type"].into_iter().collect();
+        if input.starts_with("type "){
+            let tocheck=input[5..].trim();
+            if builtin_cmds.contains(tocheck){
+                println!("{} is a shell builtin", tocheck);
+            }
+            else{
+                println!("{}: not found", tocheck);
+            }
         }
         else if input.starts_with("echo "){
             let toout=input[5..].trim();
